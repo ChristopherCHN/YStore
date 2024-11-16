81cf57d91d32ccef9f8f0431fdae6892f5c4f1da
ChristopherCHN <zhhaox@outlook.com>
2024-11-16 21:02:06 +0800

    Change on 16 Nov 2024

---
+++ src/test/java/com/cy/store/mapper/AddressMapperTests.java
@@ -0,0 +1,43 @@
+package com.cy.store.mapper;
+
+import com.cy.store.entity.Address;
+import com.cy.store.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+//@SpringBootTest注解 表示标注当前的类是一个测试类，不会随同项目一起打包发送
+@SpringBootTest
+//@RunWith注解 表示启动这个单元测试类。
+@RunWith(SpringRunner.class)
+public class AddressMapperTests {
+    //老版本的idea有检测功能，接口是不能够直接创建Bean的（动态代理技术来解决）
+    //没报错就不用管
+    @Autowired
+    private AddressMapper addressMapper;
+    /**
+     * 单元测试方法要想单独独立运行（不用启动整个项目即可进行单元测试），必须同时满足的特点如下：
+     * 1 必须被Test注解所修饰
+     * 2 返回值类型必须是void
+     * 3 方法的参数列表不能指定任何类型
+     * 4 方法的访问修饰符必须是public
+     */
+    @Test
+    public void insert() {
+        Address address = new Address();
+        // aid自增，不要赋值
+        address.setUid(8);
+        address.setName("女朋友");
+        address.setPhone("10020030000");
+        addressMapper.insert(address);
+    }
+
+    @Test
+    public void countByUid() {
+        Integer uid = 8;
+        Integer count = addressMapper.countByUid(uid);
+        System.out.println(count);
+    }
+}
